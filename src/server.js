433435937dc7a7ff71c3f33c2decfbd9fef8309f
magiccoder <shemaredwards1990@gmail.com>
2021-12-22 18:34:02 -0500

    updated

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/MoneyTransferFrontend'));
+app.use(express.static('./dist/money-transfer-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/MoneyTransferFrontend/'}),
+    res.sendFile('index.html', {root: 'dist/money-transfer-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
